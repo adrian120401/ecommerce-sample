f4a03f73165b1f15ec9085c14da21924500d77d3
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2024-04-03 02:08:04 -0300

    init project and add navbar

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { MainNav } from "@/components/main-nav"
+
+export default function Home() {
+  return (
+    <main className="">
+        <MainNav />
+    </main>
+  );
+}
---
+++ frontend/components/main-nav.tsx
@@ -0,0 +1,80 @@
+'use client'
+import {Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenuItem, NavbarMenu, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import { useState } from "react";
+
+export const MainNav: React.FC = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  const menuItems = [
+    "Profile",
+    "Categories",
+    "Shopping Cart",
+    "Login",
+    "Sign Up",
+  ];
+
+  return (
+    <Navbar
+      isBordered
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
+      className="bg-gray-700 text-white"
+    >
+      <NavbarContent className="sm:hidden" justify="start">
+        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden pr-3" justify="center">
+        <NavbarBrand>
+          <p className="font-bold">Ecommerce</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarBrand>
+          <p className="font-bold">Ecommerce</p>
+        </NavbarBrand>
+        <NavbarItem>
+          <Link className="text-slate-200" href="#">
+            Categories
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link className="text-slate-200" href="#">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link className="text-slate-200" href="#">
+            About us
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="secondary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+};
